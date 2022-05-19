24d3d81f919ec50f22d25338cd09fd4410ba03ab
Manpreet <manpreet.mankoo27@gmail.com>
2022-05-19 15:17:30 +0530

    Rectified test data json file path

--- src/main/java/com/tmb/constants/FrameworkConstants.java
+++ src/main/java/com/tmb/constants/FrameworkConstants.java
@@ -5,7 +5,7 @@ public final class FrameworkConstants {
 	private FrameworkConstants() {}
 
 	private static final String RESOURCESPATH = System.getProperty("user.dir")+"/src/test/resources";
-	private static final String CREATEUSERRESPONSESCHEMAFILEPATH = RESOURCESPATH + "responsejsonschemas/user.json";
+	private static final String CREATEUSERRESPONSESCHEMAFILEPATH = RESOURCESPATH + "/responsejsonschemas/user.json";
 
 	public static String getCreateUserResponseSchemaFilePath() {
 		return CREATEUSERRESPONSESCHEMAFILEPATH;
